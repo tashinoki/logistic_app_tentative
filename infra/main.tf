4d6bb9e5c8ce5519affb5766d10aeeee6cbed2bc
tashinoki <kenya.112163@gmail.com>
2025-04-20 10:35:15 +0900

    create firestore

---
+++ infra/main.tf
@@ -0,0 +1,9 @@
+# Firestore モジュールの呼び出し
+# modules/firestore ディレクトリを参照します
+module "firestore" {
+  source     = "./modules/firestore"
+  project_id = var.project_id # ルートで定義したproject_idを渡す
+  region     = var.region     # ルートで定義したregionを渡す (Firestoreのlocationに使うなど)
+  # Firestoreモジュール固有の変数があればここで渡す
+  # database_type = "NATIVE"
+}
