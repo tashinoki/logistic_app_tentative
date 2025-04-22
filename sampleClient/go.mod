42ab02fa2cb37a4429cba8923ac72a22e69c07bc
tashinoki <kenya.112163@gmail.com>
2025-04-22 21:44:41 +0900

    add grpc server

--- sampleClient/go.mod
+++ sampleClient/go.mod
@@ -4,5 +4,6 @@ go 1.23.5
 
 require (
 	github.com/bufbuild/connect-go v1.10.0
+	github.com/go-chi/chi v1.5.5
 	google.golang.org/protobuf v1.36.6
 )
--- sampleClient/main.go
+++ sampleClient/main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"net/http"
+	"sampleClient/gen/go-grpc/api/eventv1connect"
+	eventv1 "sampleClient/gen/go/api"
+
+	connect "github.com/bufbuild/connect-go"
+	"github.com/go-chi/chi"
+)
+
+type client struct{}
+
+func (c *client) PublishEvent(
+	ctx context.Context,
+	req *connect.Request[eventv1.PublishEventRequest],
+) (*connect.Response[eventv1.PublishEventResponse], error) {
+
+	res := connect.NewResponse(&eventv1.PublishEventResponse{
+		Success: true,
+	})
+	return res, nil
+}
+
+func main() {
+
+	mux := chi.NewRouter()
+
+	client := &client{}
+	path, handler := eventv1connect.NewEventServiceHandler(client)
+	log.Printf("Registering handler for path: %s", path)
+	mux.Mount(path, handler)
+
+	log.Println("Starting connect-gRPC server on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
+}
